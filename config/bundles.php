b43f3fbaf01b10c70b138d45aa54dc7d8b40fe6c
Alex Rock Ancelet <pierstoval@gmail.com>
2021-10-17 21:01:56 +0200

    Upgrade to Symfony 5.3 by recreating the whole SF app

--- config/bundles.php
+++ config/bundles.php
@@ -3,8 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['dev' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
